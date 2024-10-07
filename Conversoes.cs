95f82a74e01002d4cb39845c716ade89f95b7c26
rapc <rapc987@gmail.com>
2024-10-07 14:50:47 +0100

    Adicionar o método Converter Distâncias

--- Conversoes.cs
+++ Conversoes.cs
@@ -18,6 +18,17 @@ namespace Calculos
             FahreneitCelcius 
 
         }
+        /// <summary>
+        /// Tipo de Conversões de distâncias
+        /// </summary>
+        public enum ConversaoDistancia
+        {
+            Nulo = 0,
+            MetrosMilhas,
+            MilhasMetros,
+
+        }
+
     /// <summary>
     /// Conversão de Temperaturas
     /// </summary>
@@ -33,6 +44,25 @@ namespace Calculos
                 else if(conversao == ConversaoTemperatura.FahreneitCelcius )
                 {
                     return ((temperatura = 1.8000 - 32)/1.8000);
+                }
+
+                return -1; 
+            }
+    /// <summary>
+    /// Conversão de Distancias
+    /// </summary>
+    /// <param name="conversao">A Conversão a efetuar</param>
+    /// <param name="distancia">A Distância a Converter</param>
+    /// <returns>Retorna o Resultado da Conversão da distância</returns>
+     public static double ConverterDistancias(ConversaoDistancia, double distancia)
+            {
+                if(conversao == ConversaoDistancia.MetrosMilhas)
+                {
+                    return (distancia * 0.0006213712);
+                }
+                if(conversao == ConversaoDistancia.MilhasMetros)
+                {
+                    return (distancia * 1609.344);
                 }
 
                 return -1; 
